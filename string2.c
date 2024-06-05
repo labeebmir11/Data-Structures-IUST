3c92fa91a6b0317355863466f24ac9b2864aa57d
labeebmir11 <121848694+labeebmir11@users.noreply.github.com>
2024-06-05 23:56:06 +0530

    Update string2.c

--- string2.c
+++ string2.c
@@ -33,7 +33,7 @@ void concat(char s1[], char s2[], char s3[])
 int compare(char s1[], char s2[])
 {   
     int i,j;
-    for(i=0,j=0; s1[i]!='\0' && s2[i]!='\0'; i++,j++)
+    for(i=0,j=0; s1[i]!='\0' && s2[j]!='\0'; i++,j++)
     {
         if(s1[i]!=s2[j])
             break;
